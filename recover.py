9fdec4b899529377e048f2ad807db3ea7607866f
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-06-19 14:35:20 +1200

    Created Lagrange class:
     * Implemented Langrange interpolation function
     * Implemented polynomial table expansion using table method
         - Implemented antidiagonal polynomial reduce function
         - Implemented polynomial table expansion using table method
         - https://en.wikipedia.org/wiki/FOIL_method#Table_as_an_alternative_to_FOIL

--- recover.py
+++ recover.py
@@ -1,3 +1,90 @@
+class Lagrange():
+    def __init__(self):
+        pass
+
+    # https://en.wikipedia.org/wiki/FOIL_method#Table_as_an_alternative_to_FOIL
+    def antidiagonal_polynomial_reduce(self, polynomial_table):
+
+        antidiagonals = []
+
+        x_len = len(polynomial_table)
+        y_len = len(polynomial_table[0])
+
+        x_dir = 1
+        y_dir = -1
+
+        x, y = 0, 0
+        antidiagonal = []
+        while True:
+
+            if (x + x_dir > x_len and y == y_len - 1) \
+            or (y + y_dir > y_len and x == x_len - 1):
+                break
+
+
+            antidiagonal.append(table[x][y])
+
+            if (x + x_dir >= x_len or x + x_dir < 0):
+                x_dir, y_dir = y_dir, x_dir
+                y   += 1
+
+                antidiagonals.append(antidiagonal)
+                antidiagonal = []
+                continue
+
+            elif (y + y_dir >= y_len or y + y_dir < 0):
+                x_dir, y_dir = y_dir, x_dir
+                x   += 1
+
+                antidiagonals.append(antidiagonal)
+                antidiagonal = []
+
+                continue
+
+            x = x + x_dir
+            y = y + y_dir
+
+        polynomial = list(map(lambda antidiagonal: sum(antidiagonal), antidiagonals))
+
+        return polynomial
+
+
+    # https://en.wikipedia.org/wiki/FOIL_method#Table_as_an_alternative_to_FOIL
+    def table_polynomial_expansion(self, polyA, polyB):
+
+        polynomial_table = []
+        for coofA in polyA:
+            row = []
+            for coofB in polyB:
+                row.append(coofA * coofB)
+
+            polynomial_table.append(row)
+
+        return self.antidiagonal_polynomial_reduce(polynomial_table)
+
+
+    def lagrange_interpolate(self, xi, points):
+
+        monomials = []
+        for point in points:
+            xj = point['id']
+            if xj != xi:
+                denominator = xi - xj
+                monomials.append([1 / denominator, xj / denominator])
+
+        return monomials
+
+
+    def recover(self, block):
+        p      = block['prime']
+        points = block['points']
+
+        for point in points:
+            point['li'] = self.lagrange_interpolate(point['id'], points)
+
+        print(points)
+
+
 class RecoverSecret():
     def __init__(self):
         self.INDEX_SIZE = 1  # 1 byte
@@ -33,8 +120,8 @@ class RecoverSecret():
             blocks_raw.append(points)
 
 
-        blocks_fomated = []
         # Formating points
+        blocks_fomated = []
         for recovery_block in blocks_raw:
             prime = ''
             points = []
@@ -52,12 +139,17 @@ class RecoverSecret():
                 points.append(point)
 
             points = sorted(points, key=lambda k: k['id'])
-            prime = int(prime, 16)
+            prime  = int(prime, 16)
 
             blocks_fomated.append({
                 'prime'  : prime,
                 'points' : points
             })
 
+        blocks_recovered = []
+        for block in blocks_fomated:
+            interpolator    = Lagrange()
+            block_recovered = interpolator.recover(block)
+            blocks_recovered.append(block_recovered)
 
         return blocks_fomated
